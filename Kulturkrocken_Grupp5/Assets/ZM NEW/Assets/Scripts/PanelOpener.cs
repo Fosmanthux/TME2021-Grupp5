8d2d077d92b68030a30043acdb5999eef14aa3a4
ZahraMoghimi <78591263+ZahraMoghimi@users.noreply.github.com>
2021-05-22 17:02:34 +0200

    new menu

---
+++ Kulturkrocken_Grupp5/Assets/ZM NEW/Assets/Scripts/PanelOpener.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject Panel; 
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            bool isActive = Panel.activeSelf; 
+
+            Panel.SetActive(!isActive);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
